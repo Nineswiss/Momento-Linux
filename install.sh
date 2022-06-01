09c55c1dcf110a28ed4a9936921bd1dbc37b7a23
Nineswiss <adam@nineswiss.com>
2022-06-01 04:25:53 +0000

    Bash fix2

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 echo "Installing Momento..."
-apt install -y xinit
-apt install -y x11-xserver-utils
-apt install -y lxterminal
-apt install -y openbox
-mkdir home/$USER/.config
-mkdir home/$USER/.config/openbox
-cp opbtheme/config/openbox home/$USER/.config
-cp -r opbtheme/Momento /usr/share/themes
-mkdir home/$USER/.config/gtk-3.0
-cp gtk/settings.ini home/$USER/.config/gtk-3.0
-mkdir home/$USER/.config/picom
-cp picom/picom.conf home/$USER/.config/picom
+sudo apt install -y xinit
+sudo apt install -y x11-xserver-utils
+sudo apt install -y lxterminal
+sudo apt install -y openbox
+sudo mkdir home/$USER/.config
+sudo mkdir home/$USER/.config/openbox
+sudo cp -r opbtheme/config/openbox home/$USER/.config
+sudo cp -r opbtheme/Momento /usr/share/themes
+sudo mkdir home/$USER/.config/gtk-3.0
+sudo cp gtk/settings.ini home/$USER/.config/gtk-3.0
+sudo mkdir home/$USER/.config/picom
+sudo cp picom/picom.conf home/$USER/.config/picom
