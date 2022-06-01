58dc54f43b9e4ac1c5b9ebbc833b61ad1fd3b512
Nineswiss <adam@nineswiss.com>
2022-06-01 04:27:33 +0000

    Bash fix3

--- install.sh
+++ install.sh
@@ -4,11 +4,11 @@ sudo apt install -y xinit
 sudo apt install -y x11-xserver-utils
 sudo apt install -y lxterminal
 sudo apt install -y openbox
-sudo mkdir home/$USER/.config
-sudo mkdir home/$USER/.config/openbox
-sudo cp -r opbtheme/config/openbox home/$USER/.config
+sudo mkdir /home/$USER/.config
+sudo mkdir /home/$USER/.config/openbox
+sudo cp -r opbtheme/config/openbox /home/$USER/.config
 sudo cp -r opbtheme/Momento /usr/share/themes
-sudo mkdir home/$USER/.config/gtk-3.0
-sudo cp gtk/settings.ini home/$USER/.config/gtk-3.0
-sudo mkdir home/$USER/.config/picom
-sudo cp picom/picom.conf home/$USER/.config/picom
+sudo mkdir /home/$USER/.config/gtk-3.0
+sudo cp gtk/settings.ini /home/$USER/.config/gtk-3.0
+sudo mkdir /home/$USER/.config/picom
+sudo cp picom/picom.conf /home/$USER/.config/picom
