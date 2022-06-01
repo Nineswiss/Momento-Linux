ca5fca16bda858afd70b9e587400e3facdd36b96
Nineswiss <adam@nineswiss.com>
2022-06-01 04:40:16 +0000

    Add Rebbot

--- install.sh
+++ install.sh
@@ -9,8 +9,10 @@ sudo mkdir /home/$USER/.config
 sudo mkdir /home/$USER/.config/openbox
 sudo cp -r opbtheme/config/openbox /home/$USER/.config
 sudo cp -r opbtheme/Momento /usr/share/themes
+sudo cp -r gtk/Nordic-darker /usr/share/themes
 sudo mkdir /home/$USER/.config/gtk-3.0
 sudo cp gtk/settings.ini /home/$USER/.config/gtk-3.0
 sudo mkdir /home/$USER/.config/picom
 sudo cp picom/picom.conf /home/$USER/.config/picom
 echo "export QT_QPA_PLATFORMTHEME=gtk2" >> ~/.profile
+reboot
