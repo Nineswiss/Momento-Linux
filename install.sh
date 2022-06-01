ca33170dc2d3e2a700fcedea390289a2bcd03c14
Nineswiss <adam@nineswiss.com>
2022-06-01 04:30:55 +0000

    Bash fix4

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ sudo apt install -y xinit
 sudo apt install -y x11-xserver-utils
 sudo apt install -y lxterminal
 sudo apt install -y openbox
+sudo apt install -y picom
 sudo mkdir /home/$USER/.config
 sudo mkdir /home/$USER/.config/openbox
 sudo cp -r opbtheme/config/openbox /home/$USER/.config
@@ -12,3 +13,4 @@ sudo mkdir /home/$USER/.config/gtk-3.0
 sudo cp gtk/settings.ini /home/$USER/.config/gtk-3.0
 sudo mkdir /home/$USER/.config/picom
 sudo cp picom/picom.conf /home/$USER/.config/picom
+echo "export QT_QPA_PLATFORMTHEME=gtk2" >> ~/.profile
