518aeea011353aa5fe35d9d5abb87eb7539d7588
Nineswiss <adam@nineswiss.com>
2022-06-01 04:13:18 +0000

    Added Shell Script Take 5

--- install.sh
+++ install.sh
@@ -4,11 +4,11 @@ apt install -y xinit
 apt install -y x11-xserver-utils
 apt install -y lxterminal
 apt install -y openbox
-sudo mkdir ~/.config
-sudo mkdir ~/.config/openbox
-sudo cp opbtheme/config/openbox ~/.config
+sudo mkdir $HOME/.config
+sudo mkdir $HOME/.config/openbox
+sudo cp opbtheme/config/openbox $HOME/.config
 sudo cp -r opbtheme/config/Momento /usr/share/themes
-sudo mkdir ~/.config/gtk-3.0
-sudo cp gtk/settings.ini ~/.config/gtk-3.0
-sudo mkdir  ~/.config/picom
-sudo cp picom/picom.conf ~/.config/picom
+sudo mkdir $HOME/.config/gtk-3.0
+sudo cp gtk/settings.ini $HOME/.config/gtk-3.0
+sudo mkdir $HOME/.config/picom
+sudo cp picom/picom.conf $HOME/.config/picom
