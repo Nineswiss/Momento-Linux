4c7eda635f02253bc387910b201c4e30363cff84
adam <adam@nineswiss.com>
2022-06-03 09:10:00 +1000

    Edited openbox config for MDesktop

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ sudo apt install -y lxterminal
 sudo apt install -y openbox
 sudo apt install -y picom
 sudo apt install hsetroot
+sudo apt install flatpak
 sudo mkdir /home/$USER/.config
 sudo mkdir /home/$USER/.config/openbox
 sudo mkdir /home/$USER/Documents
@@ -22,5 +23,9 @@ sudo mkdir /home/$USER/.config/gtk-3.0
 sudo cp gtk/settings.ini /home/$USER/.config/gtk-3.0
 sudo mkdir /home/$USER/.config/picom
 sudo cp picom/picom.conf /home/$USER/.config/picom
+sudo mkdir /home/$USER/.config/momento
+sudo touch /home/$USER/.config/momento/desktop.json
+sudo chmod 777 /home/$USER/.config/momento/desktop.json
+sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 echo "export QT_QPA_PLATFORMTHEME=gtk2" >> ~/.profile
 reboot
