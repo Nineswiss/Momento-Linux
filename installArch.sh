6e1fb7606d5949b129e0b3dfb3b314564c371258
Nineswiss <adam@nineswiss.com>
2022-06-17 12:16:17 +1000

    Arch Install Change Sudo

--- installArch.sh
+++ installArch.sh
@@ -10,26 +10,26 @@ sudo pacman -S -y picom
 sudo pacman -S hsetroot
 cp /etc/X11/xinit/xinitrc ~/.xinitrc
 echo "exec openbox-session &" >> ~/.xinitrc
-sudo mkdir /home/$USER/.config
-sudo mkdir /home/$USER/.config/openbox
-sudo mkdir /home/$USER/Documents
-sudo mkdir /home/$USER/Downloads
-sudo mkdir /home/$USER/Pictures
-sudo mkdir /home/$USER/Pictures/Wallpaper
-sudo mkdir /home/$USER/Music
-sudo mkdir /home/$USER/Videos
-sudo mkdir /home/$USER/Desktop
+mkdir /home/$USER/.config
+mkdir /home/$USER/.config/openbox
+mkdir /home/$USER/Documents
+mkdir /home/$USER/Downloads
+mkdir /home/$USER/Pictures
+mkdir /home/$USER/Pictures/Wallpaper
+mkdir /home/$USER/Music
+mkdir /home/$USER/Videos
+mkdir /home/$USER/Desktop
 sudo chmod 777 /home/$USER/Documents /home/$USER/Downloads /home/$USER/Pictures /home/$USER/Pictures/Wallpaper /home/$USER/Music /home/$USER/Videos  /home/$USER/Desktop
-sudo cp wallpaper/bright.jpg /home/$USER/Pictures/Wallpaper
-sudo cp -r opbtheme/config/openbox /home/$USER/.config
-sudo cp -r opbtheme/Momento /usr/share/themes
-sudo cp -r gtk/Nordic-darker /usr/share/themes
-sudo mkdir /home/$USER/.config/gtk-3.0
-sudo cp gtk/settings.ini /home/$USER/.config/gtk-3.0
-sudo mkdir /home/$USER/.config/picom
-sudo cp picom/picom.conf /home/$USER/.config/picom
-sudo mkdir /home/$USER/.config/momento
-sudo touch /home/$USER/.config/momento/desktop.json
+cp wallpaper/bright.jpg /home/$USER/Pictures/Wallpaper
+cp -r opbtheme/config/openbox /home/$USER/.config
+cp -r opbtheme/Momento /usr/share/themes
+cp -r gtk/Nordic-darker /usr/share/themes
+mkdir /home/$USER/.config/gtk-3.0
+cp gtk/settings.ini /home/$USER/.config/gtk-3.0
+mkdir /home/$USER/.config/picom
+cp picom/picom.conf /home/$USER/.config/picom
+mkdir /home/$USER/.config/momento
+touch /home/$USER/.config/momento/desktop.json
 sudo chmod 777 /home/$USER/.config/momento/desktop.json
 echo "export QT_QPA_PLATFORMTHEME=gtk2" >> ~/.profile
 sudo chmod 777 /home/$USER/Music /home/$USER/Pictures/ /home/$USER/Videos/ /home/$USER/Documents/ /home/$USER/.config
