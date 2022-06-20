1226a943f2fa9edfda83dd8c388d615cf40dcdff
Nineswiss <adam@nineswiss.com>
2022-06-20 15:15:16 +1000

    Arch Add Battery Percent

--- installArch.sh
+++ installArch.sh
@@ -8,6 +8,7 @@ sudo pacman -S --noconfirm openbox
 sudo pacman -S --noconfirm picom
 sudo pacman -S --noconfirm hsetroot
 sudo pacman -S --noconfirm polkit
+sudo pacman -S --noconfirm upower
 cp xinit/xinitrc ~/.xinitrc
 mkdir /home/$USER/.config
 mkdir /home/$USER/.config/openbox
