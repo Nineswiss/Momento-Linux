309f29f8d71de44446c92138efb92d897119627d
Nineswiss <adam@nineswiss.com>
2022-07-09 14:18:14 +1000

    Fixed Appudater paths

--- scripts/AppLinkUpdate.sh
+++ scripts/AppLinkUpdate.sh
@@ -4,9 +4,14 @@ for f in $FILES
 do
   echo "Processing $f file..."
   ln -s $f "$LINKDEDIR"
+  # take action on each file. $f store current file name
+#   cat "$f"
 done
-FILESSNAP="/var/lib/snapd/desktop/applications/*"
+FILESSNAP="/var/lib/flatpak/exports/share/applications/*"
 for f in $FILESSNAP
 do
+  echo "Processing $f file..."
   ln -s $f "$LINKDEDIR"
+  # take action on each file. $f store current file name
+#   cat "$f"
 done
--- scripts/Applink.sh
+++ scripts/Applink.sh
@@ -1,9 +1,11 @@
-inotifywait -m -r /usr/share/applications /var/lib/snapd/desktop/applications -e create -e moved_to -e delete|
+inotifywait -m -r /usr/share/applications /var/lib/flatpak/exports/share/applications -e create -e moved_to -e delete|
     while read dir action file; do
         echo $action
         FILEPATH="$dir$file"
         LINKDEDIR=$HOME/Applications
         if  [ "$action" == "CREATE" ]; then
+            ln -s $FILEPATH "$LINKDEDIR/$file"
+        elif  [ "$action" == "MOVED_TO" ]; then
             ln -s $FILEPATH "$LINKDEDIR/$file"
         elif  [ "$action" == "DELETE" ]; then
             unlink "$LINKDEDIR/$file"
