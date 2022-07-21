71bb7fb2d1912d4e0588de44d723dafed76910ab
adam <adam@nineswiss.com>
2022-07-21 14:25:09 +1000

    fixed applink scripts

--- scripts/AppLinkUpdate.sh
+++ scripts/AppLinkUpdate.sh
@@ -2,16 +2,20 @@ FILES="/usr/share/applications/*"
 LINKDEDIR=$HOME/Applications
 for f in $FILES
 do
+if [[ $f == *.desktop ]]; then
   echo "Processing $f file..."
   ln -s $f "$LINKDEDIR"
+fi
   # take action on each file. $f store current file name
 #   cat "$f"
 done
-FILESSNAP="/var/lib/flatpak/exports/share/applications/*"
-for f in $FILESSNAP
+FILESFLAT="/var/lib/flatpak/exports/share/applications/*"
+for f in $FILESFLAT
 do
+  if [[ $f == *.desktop ]]; then
   echo "Processing $f file..."
   ln -s $f "$LINKDEDIR"
+fi
   # take action on each file. $f store current file name
 #   cat "$f"
 done
--- scripts/Applink.sh
+++ scripts/Applink.sh
@@ -4,9 +4,13 @@ inotifywait -m -r /usr/share/applications /var/lib/flatpak/exports/share/applica
         FILEPATH="$dir$file"
         LINKDEDIR=$HOME/Applications
         if  [ "$action" == "CREATE" ]; then
+            if [[ $file == *.desktop ]]; then
             ln -s $FILEPATH "$LINKDEDIR/$file"
+            fi
         elif  [ "$action" == "MOVED_TO" ]; then
+            if [[ $file == *.desktop ]]; then
             ln -s $FILEPATH "$LINKDEDIR/$file"
+            fi
         elif  [ "$action" == "DELETE" ]; then
             unlink "$LINKDEDIR/$file"
         fi
