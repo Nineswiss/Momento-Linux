6b62fa6328519b8ded5e0ea9152a722b3fba88b8
Nineswiss <adam@nineswiss.com>
2022-09-20 11:24:44 +1000

    InstallFedKwin

---
+++ installFedKwin.sh
@@ -0,0 +1,12 @@
+sudo dnf install -y @base-x
+sudo dnf install -y nano
+sudo dnf install -y adobe-source-code-pro-fonts
+sudo dnf install -y kwin-x11
+sudo dnf install -y lxterminal
+sudo dnf install -y qt6-*
+sudo dnf install -y xrandr
+sudo dnf install -y xdotool
+sudo dnf install -y xprop
+sudo dnf install -y wmctrl
+git clone https://github.com/Nineswiss/Momento-Linux
+cp Momento-Linux/xinitrc ~/.xinitrc
