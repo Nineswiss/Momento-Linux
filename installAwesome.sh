47efca44ad52a0f94741c3c2c03e61e9d6db02be
Nineswiss <adam@nineswiss.com>
2022-09-21 09:02:40 +1000

    AWM

--- installAwesome.sh
+++ installAwesome.sh
@@ -1,1 +1,34 @@
-sudo apt install cmake libgdk-pixbuf2.0-dev libcairo2-dev libxcb-cursor-dev libxcb-randr0-dev libxcb-xtest0-dev libxcb-xinerama0-dev libxcb-shape0-dev libxcb-util-dev libxcb-keysyms1-dev libxcb-icccm4-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev libstartup-notification0-dev libxdg-basedir-dev libxcb-xrm-dev lua5.2 liblua5.2-dev lua-lgi-dev
+sudo apt-get install --no-install-recommends \
+    asciidoctor \
+    cmake \
+    dbus-x11 \
+    gettext \
+    gir1.2-gtk-3.0 \
+    gir1.2-pango-1.0 \
+    git \
+    libdbus-1-dev \
+    libgirepository1.0-dev \
+    libnotify-bin \
+    libpango1.0-dev \
+    libstartup-notification0-dev \
+    libx11-xcb-dev \
+    libxcb-cursor-dev \
+    libxcb-icccm4-dev \
+    libxcb-keysyms1-dev \
+    libxcb-randr0-dev \
+    libxcb-shape0-dev \
+    libxcb-util0-dev \
+    libxcb-xfixes0-dev \
+    libxcb-xinerama0-dev \
+    libxcb-xkb-dev \
+    libxcb-xrm-dev \
+    libxcb-xtest0-dev \
+    libxdg-basedir-dev \
+    libxkbcommon-dev \
+    libxkbcommon-x11-dev \
+    xutils-dev \
+    liblua5.3-dev \
+    lua5.3
+
+luarocks install lgi
+luarocks install ldoc
