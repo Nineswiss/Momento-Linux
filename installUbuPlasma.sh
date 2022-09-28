fa9db35dd9505179f6a12b3f5e9c840b40a659bd
Nineswiss <adam@nineswiss.com>
2022-09-29 09:54:29 +1000

    Update

--- installUbuPlasma.sh
+++ installUbuPlasma.sh
@@ -6,6 +6,7 @@
 # sudo apt install -y wmctrl
 # sudo apt install -y sddm
 sudo apt install -y make cmake extra-cmake-modules qtdeclarative5-dev libkf5plasma-dev libqt5x11extras5-dev g++ libsm-dev libkf5configwidgets-dev libkdecorations2-dev libxcb-randr0-dev libkf5wayland-dev plasma-workspace-dev
+cd ..
 git clone https://github.com/psifidotos/applet-window-appmenu.git
 cd applet-window-appmenu
 sh install.sh
