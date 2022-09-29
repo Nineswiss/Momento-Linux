9fbf1f99791c25ff6a4c7e24757c24e62bbaeec6
Nineswiss <adam@nineswiss.com>
2022-09-29 15:50:59 +1000

    FedPlasInstall

--- installFedKwin.sh
+++ installFedKwin.sh
@@ -1,26 +1,37 @@
-# sudo dnf install -y @base-x
-# sudo dnf install -y nano
-# sudo dnf install -y adobe-source-code-pro-fonts
-# sudo dnf install -y lxterminal
-# sudo dnf install -y qt6-*
-# sudo dnf install -y xrandr
-# sudo dnf install -y xdotool
-# sudo dnf install -y xprop
-# sudo dnf install -y wmctrl
-# sudo dnf install -y plasma-desktop
-# sudo dnf install -y plasma-workspace-devel
-# sudo dnf install -y sddm
-# sudo dnf install -y unity-gtk-module-common unity-gtk2-module unity-gtk3-module
+sudo dnf install -y @base-x
+sudo dnf install -y nano
+sudo dnf install -y adobe-source-code-pro-fonts
+sudo dnf install -y lxterminal
+sudo dnf install -y qt6-*
+sudo dnf install -y xrandr
+sudo dnf install -y xdotool
+sudo dnf install -y xprop
+sudo dnf install -y wmctrl
+sudo dnf install -y plasma-desktop
+sudo dnf install -y plasma-workspace-devel
+sudo dnf install -y sddm
+sudo dnf install -y make cmake extra-cmake-modules qt5-qtdeclarative-devel kf5-plasma-devel qt5-qtx11extras-devel gcc-c++ libSM-devel kf5-kconfigwidgets-devel kdecoration-devel kf5-kitemmodels-devel plasma-workspace-devel kf5-kwayland-devel
+sudo dnf install -y unity-gtk-module-common unity-gtk2-module unity-gtk3-module
 
-# sudo systemctl enable sddm.service -f
-# sudo systemctl set-default graphical.target 
-# sudo systemctl enable sddm --force
-# systemctl start sddm.service
-# reboot
+cd
+git clone https://github.com/Nineswiss/applet-window-appmenu
+cd applet-window-appmenu
+sh install.sh
+cd 
+git clone https://github.com/Nineswiss/applet-window-title
+cd applet-window-title
+plasmapkg2 -i .
+
+
+sudo systemctl enable sddm.service -f
+sudo systemctl set-default graphical.target 
+sudo systemctl enable sddm --force
+sudo systemctl start sddm.service
+reboot
 
 # APPLET WINDOW MENU
 # https://github.com/psifidotos/applet-window-appmenu/blob/master/INSTALLATION.md
-sudo dnf install -y make cmake extra-cmake-modules qt5-qtdeclarative-devel kf5-plasma-devel qt5-qtx11extras-devel gcc-c++ libSM-devel kf5-kconfigwidgets-devel kdecoration-devel kf5-kitemmodels-devel plasma-workspace-devel kf5-kwayland-devel 
+# sudo dnf install -y make cmake extra-cmake-modules qt5-qtdeclarative-devel kf5-plasma-devel qt5-qtx11extras-devel gcc-c++ libSM-devel kf5-kconfigwidgets-devel kdecoration-devel kf5-kitemmodels-devel plasma-workspace-devel kf5-kwayland-devel 
 
 # git clone https://github.com/Nineswiss/Momento-Linux
 # cp ~/Momento-Linux/xinitrc ~/.xinitrc
--- installUbuPlasma.sh
+++ installUbuPlasma.sh
@@ -5,14 +5,26 @@
 # sudo apt install -y xprop
 # sudo apt install -y wmctrl
 # sudo apt install -y sddm
+sudo apt install -y plocate
+sudo apt install spice-vdagent spice-webdavd
 sudo apt install -y make cmake extra-cmake-modules qtdeclarative5-dev libkf5plasma-dev libqt5x11extras5-dev g++ libsm-dev libkf5configwidgets-dev libkdecorations2-dev libxcb-randr0-dev libkf5wayland-dev plasma-workspace-dev
-cd ..
+sudo apt install -y qt6-tools-dev
+sudo apt install -y qt6-tools-dev-tools
+sudo apt install -y qt6-declarative-dev
+sudo apt install -y qt6-declarative-private-dev
+sudo apt install -y qt6-base-private-dev
+sudo apt install -y qml6-module-qtquick
+sudo apt install -y qml6-module-qtquick-window
+cd
 git clone https://github.com/psifidotos/applet-window-appmenu.git
 cd applet-window-appmenu
 sh install.sh
-
+cd
+git clone https://github.com/psifidotos/applet-window-title
+cd applet-window-title
+kpackagetool5 -i .
 # sudo systemctl enable sddm.service -f
 # sudo systemctl set-default graphical.target 
-# sudo systemctl enable sddm --force
+# sudo systemctl enable sddm --forcec
 # systemctl start sddm.service
 # reboot
