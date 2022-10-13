c04a92b5e1015c26d2103ab78b82344c339598d9
Nineswiss <adam@nineswiss.com>
2022-10-13 16:29:31 +1100

    IconFinder

--- scripts/iconFinder.sh
+++ scripts/iconFinder.sh
@@ -3,9 +3,11 @@ Folders="/var/lib/flatpak/exports/share/icons/hicolor/96x96/
 /var/lib/flatpak/exports/share/icons/hicolor/64x64/ 
 /var/lib/flatpak/exports/share/icons/hicolor/72x72/ 
 /usr/share/icons/hicolor/scalable/apps/
-/usr/share/icons/hicolor/96x96/ 
+/usr/share/icons/hicolor/96x96/
 /usr/share/icons/hicolor/128x128/ 
 /usr/share/icons/hicolor/64x64/ 
 /usr/share/icons/hicolor/72x72/ 
-/usr/share/pixmaps/"
+/usr/share/icons/hicolor/48x48/apps/
+/usr/share/pixmaps/
+/usr/share/icons/breeze/apps/48/"
 echo "$( find $Folders \( $1 \) )" 
