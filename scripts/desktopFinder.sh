ff51aa20ba161da06c1dd560d76871b019d02477
Nineswiss <adam@nineswiss.com>
2022-10-13 16:27:18 +1100

    Bits

--- scripts/desktopFinder.sh
+++ scripts/desktopFinder.sh
@@ -1,17 +1,11 @@
-# find ~/Applications -iname *$1* -exec grep "Icon" {} \;
-# echo "$( find /var/lib/flatpak/exports/share/icons/hicolor /usr/share/pixmaps/  /usr/share/icons/hicolor/  \( -iname "*qtcreator*"  -iname "*qtproject*" \) )"
-# LIST="-iname *google* -iname *chrome*"
-
-# echo "$( find /var/lib/flatpak/exports/share/icons/hicolor /usr/share/pixmaps/  /usr/share/icons/hicolor/  \( $1 \) )"
-
-# icons=$( find ~/Applications \( $1 \) -exec grep "Icon" {} \; )
-# exec=$( find ~/Applications \( $1 \) -exec grep "Exec" {} \; )
-# echo $icons
-# echo $exec
 snap=$( awk "/X-SnapInstanceName=/" $1 )
+flat=$( awk "/X-Flatpak=/" $1 )
 if [ "$snap" != "" ]; then
 ogexe=$(echo $snap | awk -F= '{print $2}' )
-exec="/snap/bin/"$ogexe 
+exec="/snap/bin/"$ogexe
+elif [ "$flat" != "" ]; then
+ogexe=$(echo $flat | awk -F= '{print $2}' )
+exec="/flatpak/"$ogexe
 else
 ogexe=$( awk "/Exec/ && !/TryExec/" $1 | head -n 1 )
 exec=$(echo $ogexe | awk -F= '{print $2}' )
